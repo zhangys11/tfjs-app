747f5f230859cc23e2b5f851f0843de2e85417e7
zhangys11 <oo@zju.edu.cn>
2022-04-21 14:05:55 +0800

    create repo

---
+++ setup.py
@@ -0,0 +1,38 @@
+# from distutils.core import setup
+from setuptools import find_packages, setup
+import pathlib
+
+setup(
+    # Application name:
+    name="tfjsa",
+    
+    # Version number (initial):
+    version="0.0.1",
+    
+    # Application author details:
+    author="Zhang",
+    author_email="oo@zju.edu.cn",
+    
+    # Packages
+    packages=["tfjsa"],
+
+    # Include additional files into the package
+    include_package_data=True,
+    
+    # Details
+    url="http://pypi.python.org/pypi/tfjsa/",
+    
+    #
+    license="LICENSE",
+    description="tfjs(Tensorflow js)-based Flask web apps.",
+    
+    # long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    
+    # Dependent packages (distributions)
+    install_requires=[
+        "flask",
+    ],
+)
+
+# To Build and Publish (for developer only), 
+# Run: python setup.py sdist bdist_wheel; twine upload dist/*
---
+++ tfjsa/run.py
@@ -0,0 +1,58 @@
+import os, sys, json, uuid
+from flask import Flask, render_template, request
+import uuid
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 # limit to 5MB
+
+# routes
+
+@app.route("/")
+@app.route("/ImageClassification")
+def ImageClassification():
+	return render_template("ImageClassification.html")
+
+@app.route("/ObjectDetection")
+def ObjectDetection():
+	return render_template("ObjectDetection.html")
+
+@app.route("/ImageSegmentation")
+def ImageSegmentation():
+	return render_template("ImageSegmentation.html")
+
+@app.route("/StyleTransfer")
+def StyleTransfer():
+	return render_template("StyleTransfer.html")
+
+@app.route("/FundusImageQualifier")
+def FundusImageQualifier():
+	return render_template("FundusImageQualifier.html")
+
+@app.route("/Leaf")
+def Leaf():
+	return render_template("Leaf.html")
+
+@app.route("/about")
+def About():
+	return "Assembled by Dr. Zhang (oo@zju.edu.cn)"
+
+'''
+The Flask dev server is not designed to be particularly secure, stable, or efficient. 
+By default it runs on localhost (127.0.0.1), change it to app.run(host="0.0.0.0") to run on all your machine's IP addresses.
+0.0.0.0 is a special value that you can't use in the browser directly, you'll need to navigate to the actual IP address of the machine on the network. You may also need to adjust your firewall to allow external access to the port.
+The Flask quickstart docs explain this in the "Externally Visible Server" section:
+    If you run the server you will notice that the server is only accessible from your own computer, not from any other in the network. This is the default because in debugging mode a user of the application can execute arbitrary Python code on your computer.
+    If you have the debugger disabled or trust the users on your network, you can make the server publicly available simply by adding --host=0.0.0.0 to the command line.
+'''
+
+import webbrowser
+from threading import Timer
+
+def open_browser():
+    webbrowser.open_new('http://localhost:5007/')
+      
+if __name__ =='__main__':
+    # use netstat -ano|findstr 5007 to check port use
+    # Timer(3, open_browser).start()
+    app.run(host="0.0.0.0", port=5007, debug = True)
+    
